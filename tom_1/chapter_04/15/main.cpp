347ae6ff0ef87946b4f6cdb8cb8ff5e5fc6f81e9
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2016-10-18 17:28:56 +0300

    4:15

---
+++ tom_1/chapter_04/15/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "sizeof( bool) == " << sizeof( bool) << std::endl;
+	std::cout << "sizeof( char) == " << sizeof( char) << std::endl;
+	std::cout << "sizeof( short int) == " << sizeof( short int) << std::endl;
+	std::cout << "sizeof( unsigned short int) == " << sizeof( unsigned short int) << std::endl;
+	std::cout << "sizeof( int) == " << sizeof( int) << std::endl;
+	std::cout << "sizeof( unsigned int) == " << sizeof( unsigned int) << std::endl;
+	std::cout << "sizeof( long int) == " << sizeof( long int) << std::endl;
+	std::cout << "sizeof( unsigned long int) == " << sizeof( unsigned long int) << std::endl;
+	std::cout << "sizeof( float) == " << sizeof( float) << std::endl;
+	//std::cout << "sizeof( long float) == " << sizeof( long float) << std::endl;
+	std::cout << "sizeof( double) == " << sizeof( double) << std::endl;
+}

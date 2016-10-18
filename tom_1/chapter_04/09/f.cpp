bf64fd504368c75be12ea45f4f199419caa80708
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2016-10-18 11:08:04 +0300

    4:09

---
+++ tom_1/chapter_04/09/f.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print( int i)
+{
+	printf( "i = %d\n", i);
+}
---
+++ tom_1/chapter_04/09/main.cpp
@@ -0,0 +1,8 @@
+void print( float f);
+
+int main()
+{
+	// C++: g++ main.cpp f.cpp
+	// C: gcc -x c main.cpp f.cpp
+	print( 3.14);
+}

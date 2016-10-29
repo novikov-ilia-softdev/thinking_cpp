98d878eeb46f96bb799b4f9ac9aff936150716e5
ilia_novikov <smartnlg.ilia.novikov@gmail.com>
2016-10-29 11:57:22 +0300

    8:16

---
+++ tom_1/chapter_08/16/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void t( int*) {}
+
+void u( const int* cip)
+{
+	// *cip = 2;
+	int i = *cip;
+	//int * ip2 = cip;
+}
+
+const char* v()
+{
+	return "result of function v()";
+}
+
+const int* const w()
+{
+	static int i;
+	return &i;
+}
+
+int main()
+{
+	int x = 0;
+	int* ip = &x;
+	const int* cip = &x;
+	t( ip);
+	//t( cip);
+	u( ip);
+	u( cip);
+	//char* cp = v();
+	const char* ccp = v();
+	//int* ip2 = w();
+	const int* const ccip = w();
+	const int* cip2 = w();
+	//*w() = 1;
+}

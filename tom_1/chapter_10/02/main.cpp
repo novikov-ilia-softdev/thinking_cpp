e380b31dd2a3eab17d93ff1bb1efb3874e0002a6
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2016-11-09 10:35:01 +0300

    10:02

---
+++ tom_1/chapter_10/02/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+/*
+int fibo( bool reset = false)
+{
+	static int prev = 0;
+	static int prevPrev = 0;
+
+	if( reset)
+	{
+		prev = 0;
+		prevPrev = 0;
+	}
+
+	if( !prev && !prevPrev)
+	{
+		prevPrev++;
+		return 0;
+	}
+
+	int next = prev + prevPrev;
+	prevPrev = prev;
+	prev = next;
+
+	return next;
+}
+*/
+
+int fibo( bool reset = false)
+{
+	static int prev = 1;
+	static int prevPrev = 0;
+	static int count = 0;
+
+	if( reset)
+	{
+		prev = 1;
+		prevPrev = 0;
+		count = 0;
+	}
+
+	if( count == 0)
+	{
+		count++;
+		return 0;
+	}
+
+	if( count == 1)
+	{
+		count++;
+		return 1;
+	}
+
+	int next = prev + prevPrev;
+	prevPrev = prev;
+	prev = next;
+
+	return next;
+}
+
+int main()
+{
+	std::cout << "fibo:" <<std::endl;
+	for( int i = 0; i < 10; i++)
+	{
+		std::cout << fibo() << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "fibo:" <<std::endl;
+	for( int i = 0; i < 10; i++)
+	{
+		!i ? std::cout << fibo( true) << " " : std::cout << fibo() << " ";
+
+
+	}
+	std::cout << std::endl;
+}

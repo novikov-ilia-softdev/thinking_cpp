d7aa3af3180dd39526660ee560aa729f5408f6df
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2016-11-21 10:47:34 +0300

    10:23

---
+++ tom_1/chapter_10/23/main.cpp
@@ -0,0 +1,8 @@
+#include "x.h"
+#include "y.h"
+
+int main()
+{
+	using namespace NamespaceY;
+	MyClass obj;
+}
---
+++ tom_1/chapter_10/23/y.h
@@ -0,0 +1,12 @@
+#ifndef Y_H
+#define Y_H
+
+namespace NamespaceY{
+
+class MyClass{
+
+};
+
+}
+
+#endif // Y_H

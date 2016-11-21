2b2b8ae467491a0dee60b514b644ebc017f23435
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2016-11-21 10:34:28 +0300

    10:22

---
+++ tom_1/chapter_10/22/main.cpp
@@ -0,0 +1,8 @@
+#include "x.h"
+#include "y.h"
+
+int main()
+{
+	NamespaceX::MyClass obj;
+	NamespaceY::MyClass obj1;
+}
---
+++ tom_1/chapter_10/22/x.h
@@ -0,0 +1,12 @@
+#ifndef X_H
+#define X_H
+
+namespace NamespaceX{
+
+class MyClass{
+
+};
+
+}
+
+#endif // X_H

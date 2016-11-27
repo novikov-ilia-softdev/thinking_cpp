d1eb8bd5c0e9aba8827728410ebf282f50071a03
smartnlgilianovikov <smartnlg.ilia.novikov@gmail.com>
2016-11-27 16:05:06 +0300

    10:32 - not finished

--- tom_1/chapter_10/32/main.cpp
+++ tom_1/chapter_10/32/main.cpp
@@ -4,8 +4,13 @@
 //extern util::Mirror mirror_4;
 extern util::Mirror mirror_1;
 util::Mirror mirror_5( &mirror_1);
+//util::Mirror mirror_5( 0);
 
 //g++ main.cpp mirror_1.cpp mirror.h
+
+//g++ -c main.cpp
+//g++ -c mirror_1.cpp
+// g++ main.o mirror_1.o
 int main()
 {
 	std::cout << mirror_5.test() << std::endl;

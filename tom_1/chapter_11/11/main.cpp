131dec5816bd2c38075a13f4bcb61ee5131589b2
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-04-06 14:21:51 +0300

    11:11

---
+++ tom_1/chapter_11/11/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void func( char ch, int i, float f, double d)
+{
+
+}
+
+int main()
+{
+	func( 'a', 2, 3.14, 2.7178);
+}
---
+++ tom_1/chapter_11/11/main.s
@@ -0,0 +1,111 @@
+	.file	"main.cpp"
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.text
+	.globl	_Z4funccifd
+	.type	_Z4funccifd, @function
+_Z4funccifd:
+.LFB1021:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, %eax
+	movl	%esi, -8(%rbp)
+	movss	%xmm0, -12(%rbp)
+	movsd	%xmm1, -24(%rbp)
+	movb	%al, -4(%rbp)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1021:
+	.size	_Z4funccifd, .-_Z4funccifd
+	.globl	main
+	.type	main, @function
+main:
+.LFB1022:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movsd	.LC0(%rip), %xmm0
+	movapd	%xmm0, %xmm1
+	movss	.LC1(%rip), %xmm0
+	movl	$2, %esi
+	movl	$97, %edi
+	call	_Z4funccifd
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1022:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1023:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L6
+	cmpl	$65535, -8(%rbp)
+	jne	.L6
+	movl	$_ZStL8__ioinit, %edi
+	call	_ZNSt8ios_base4InitC1Ev
+	movl	$__dso_handle, %edx
+	movl	$_ZStL8__ioinit, %esi
+	movl	$_ZNSt8ios_base4InitD1Ev, %edi
+	call	__cxa_atexit
+.L6:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1023:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I__Z4funccifd, @function
+_GLOBAL__sub_I__Z4funccifd:
+.LFB1024:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1024:
+	.size	_GLOBAL__sub_I__Z4funccifd, .-_GLOBAL__sub_I__Z4funccifd
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z4funccifd
+	.section	.rodata
+	.align 8
+.LC0:
+	.long	3978857703
+	.long	1074118157
+	.align 4
+.LC1:
+	.long	1078523331
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits

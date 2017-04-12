fc9a3a34c668e844d0a772fcda7a2e23be0f212c
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-04-12 17:48:54 +0300

    12:03

---
+++ tom_1/chapter_12/03/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Int{
+public:
+	Int( int i = 0): i_( i) {}
+	const Int operator+(const Int& right) const;
+	const Int operator-(const Int& right) const;
+	void print( std::ostream& stream);
+
+private:
+	int i_;
+};
+
+const Int Int::operator+(const Int& right) const
+{
+	return Int( i_ + right.i_);
+}
+
+const Int Int::operator-(const Int& right) const
+{
+	return Int( i_ - right.i_);
+}
+
+void Int::print( std::ostream& stream)
+{
+	stream << i_ << std::endl;
+}
+
+int main()
+{
+	Int a = 5;
+	Int b = 3;
+	Int c = 1;
+	Int d = a + b - c;
+	d.print( std::cout);
+}

93127f2ab4587a57535212bd4e7b17fce3c90318
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-04-26 16:14:36 +0300

    13:05

---
+++ tom_1/chapter_13/05/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include "counted.h"
+
+int main()
+{
+	std::vector<Counted*> vector;
+
+	for( int i = 0; i < 5; i++)
+	{
+		vector.push_back( new Counted());
+	}
+
+	for( int i = 0; i < vector.size(); i++)
+	{
+		vector[ i]->f();
+	}
+
+	for( int i = 0; i < vector.size(); i++)
+	{
+		delete vector[ i];
+	}
+
+	vector.clear();
+
+	std::cout << "vector.size(): " << vector.size() << std::endl;
+}

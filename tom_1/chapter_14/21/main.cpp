827e43b740c8e9277b69c2cadf3a31a6465d1ef1
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-05-04 10:52:52 +0300

    14:21

---
+++ tom_1/chapter_14/21/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+enum note { middleC, Csharp, Cflat };
+
+class Instrument{
+public:
+	void play( note) const { std::cout << "Intrument::play" << std::endl; }
+	void prepare( note) const { std::cout << "Intrument::prepare" << std::endl; }
+};
+
+class Wind: public Instrument{
+
+};
+
+void tune( Instrument& i)
+{
+	i.play( middleC);
+	i.prepare( middleC);
+}
+
+int main()
+{
+	Wind flute;
+	tune( flute);
+}

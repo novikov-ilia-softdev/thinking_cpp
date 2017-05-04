099603f68c2d4203ee3d663ac4b772a2b82e8110
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-05-04 10:29:12 +0300

    14:18

---
+++ tom_1/chapter_14/18/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Base{
+public:
+	Base( int i = 0): i_( i) {}
+	int value( int m) { return m * i_; }
+
+protected:
+	int read() const { return i_; }
+	void set( int i) { i_ = i; }
+
+private:
+	int i_;
+};
+
+class Derived: public Base{
+public:
+	Derived( int j = 0): j_( j) {}
+	void change( int x) { set( x); }
+	int get() const { return read(); }
+
+private:
+	int j_;
+};
+
+int main()
+{
+	Derived d;
+	std::cout << d.get() << std::endl;
+	d.change( 10);
+	std::cout << d.get() << std::endl;
+}
